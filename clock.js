5e2008adf5770fbdd5ff1fda2d49cb3a5fa86f77
kim1210 <69778811+kim1210@users.noreply.github.com>
2021-03-21 14:34:48 +0900

    second commit : saveName

--- clock.js
+++ clock.js
@@ -1,6 +1,8 @@
+// 필요한 변수를 지정한다. (html에서 클래스의 이름 또는 태그를 가져온다.)
 const clockContainer = document.querySelector(".js-clock"), 
   clockTitle = clockContainer.querySelector("h1");
 
+// 현재 시간을 나타내는 함수 
 function getTime(){
     const date = new Date();
     const minutes = date.getMinutes();
@@ -11,6 +13,7 @@ function getTime(){
      }:${seconds < 10 ? `0${seconds}` : seconds}`;
 }
 
+// 실행시 수행할 함수
 function init(){
     getTime();
     setInterval(getTime, 1000);
